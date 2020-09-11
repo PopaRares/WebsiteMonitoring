c57811c1d05497a65fbbf5526b710ed98d3cb04c
Rares <raresp98@gmail.com>
2020-09-11 19:47:26 +0300

    Revamped project structure

--- script.py
+++ script.py
@@ -5,33 +5,39 @@ import smtplib # email capabilities
 import os # to import ENV variables
 
 #url = os.environ['URL']
-wait_time = int(os.environ['WAIT'])
-
 url = 'https://ac.utcluj.ro/cazare.html'
+wait_time = int(os.environ['WAIT'])
 
 #to trick website that we are a real person
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
+#to trick SINU into giving handsahke
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
-while True:
-    response = requests.get(url, headers=header, verify=False)
-    soup = BeautifulSoup(response.text, "lxml")
-    soup_hash = hash(soup)
 
-    #check if previous hash has been set, init if so
-    try:
-        prev_hash
-    except NameError:
-        prev_hash = soup_hash
+def sendEmail():
+    print("test")
 
-    if soup_hash != prev_hash:
-        print('They be different!')
-        #send email
-        prev_hash = soup_hash
-        break
-    else:
-        print('They be the same')
-        time.sleep(wait_time)
-        continue
+def main():
+    sendEmail()
+    while True:
+        response = requests.get(url, headers=header, verify=False)
+        soup = BeautifulSoup(response.text, "lxml")
+        soup_hash = hash(soup)
+    
+        #check if previous hash has been set, init if so
+        try:
+            prev_hash
+        except NameError:
+            prev_hash = soup_hash
+    
+        if soup_hash != prev_hash:
+            print('They be different!')
+            sendEmail()
+            prev_hash = soup_hash
+            break
+        else:
+            print('They be the same')
+            time.sleep(wait_time)
+            continue
 
-
+if __name__ == "__main__":
+    main()
