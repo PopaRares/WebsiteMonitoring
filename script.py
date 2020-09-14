b6b2df48489eaff70144ac6bb5ea7ae1c74b9c81
Rares <raresp98@gmail.com>
2020-09-14 21:20:51 +0300

    Modified to work with lists of emails. Added KEEP_ALIVE env variable. Modified internal prints. Modified email contents.

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ import os # to import ENV variables
 
 url = os.environ['URL']
 wait_time = int(os.environ['WAIT'])
-destination_email = os.environ['DESTINATION']
+keep_alive = os.environ['KEEP_ALIVE']
+destination_email = os.environ['DESTINATION'].split(" ")
 
 #to trick website that we are a real person
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -19,35 +20,34 @@ port = 465
 smtp_server = "smtp.gmail.com"
 
 def sendEmail():
-    #message = "The website " + url + " seems to have changed its state."
-    message = "Check the website!"
-    print(message)
+    message = 'The website {} has been recently modified.'.format(url)
+    content = 'Subject: {}\n\n{}'.format('Website Activity', message)
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, destination_email, message)
+        server.sendmail(sender_email, destination_email, content)
 
 
 def main():
-    sendEmail()
     while True:
         response = requests.get(url, headers=header, verify=False)
         soup = BeautifulSoup(response.text, "lxml")
         soup_hash = hash(soup)
     
-        #check if previous hash has been set, init if so
+        #check if previous hash has been set, init otherwise 
         try:
             prev_hash
         except NameError:
             prev_hash = soup_hash
     
         if soup_hash != prev_hash:
-            print('They be different!')
+            print('Change detected. Sending email.')
             sendEmail()
             prev_hash = soup_hash
-            break
+            if keep_alive != 'True':
+                break
         else:
-            print('They be the same')
+            print('No changes detected. Sleeping.')
             time.sleep(wait_time)
             continue
 
